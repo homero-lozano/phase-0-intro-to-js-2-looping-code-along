bfc98e67a94e49258b83c23073fde1e828127992
Nico Lozano <homeronicolaslozano@gmail.com>
2024-02-06 17:50:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki",]  
+function writeCards(cards) { 
+    const messages = []
+    for(let i = 0; i < cards.length; i++) {
+        console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`); 
+        messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return messages; 
+}
+writeCards(names);  
+function countDown() {
+    let countDown = 0 
+    while (countDown < 11) {
+        console.log(countDown++)
+    }
+}
